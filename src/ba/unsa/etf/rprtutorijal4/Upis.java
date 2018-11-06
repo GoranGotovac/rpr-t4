f6702524199b21036086f90069667dce9f39d372
student <ggotovac1@etf.unsa.ba>
2018-11-06 18:03:53 +0100

    Konstruktor klase Upis

--- src/ba/unsa/etf/rprtutorijal4/Upis.java
+++ src/ba/unsa/etf/rprtutorijal4/Upis.java
@@ -1,4 +1,13 @@
 package ba.unsa.etf.rprtutorijal4;
 
 public class Upis {
+    public Student student;
+    public Predmet predmet;
+    public PlanStudija planStudija;
+    public Upis(Student student, Predmet predmet, PlanStudija planStudija) {
+        this.student = student;
+        this.predmet = predmet;
+        this.planStudija = planStudija;
+    }
+
 }
