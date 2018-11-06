4e0c1082bc1142963e605776dbdca0131ff5fa13
student <ggotovac1@etf.unsa.ba>
2018-11-06 17:57:03 +0100

    Napravljena klasa PlanStudija i klasa Upis

---
+++ src/ba/unsa/etf/rprtutorijal4/PlanStudija.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rprtutorijal4;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class PlanStudija {
+    Map<Integer, ArrayList<Predmet>> mapa;
+
+}
--- src/ba/unsa/etf/rprtutorijal4/Predmet.java
+++ src/ba/unsa/etf/rprtutorijal4/Predmet.java
@@ -4,16 +4,18 @@ public class Predmet {
     public String nazivPredmeta;
     public int brojECTS;
     public boolean obavezni;
-    public Predmet(String ime, int broj, boolean obavezni) {}
+    public Predmet(String nazivPredmeta, int brojECTS, boolean obavezni) {
+        this.nazivPredmeta = nazivPredmeta;
+        this.obavezni = obavezni;
+        this.brojECTS = brojECTS;
+    }
 
     public String getNazivPredmeta() {
         return nazivPredmeta;
     }
-
     public int getBrojECTS() {
         return brojECTS;
     }
-
     public boolean isObavezni() {
         return obavezni;
     }
