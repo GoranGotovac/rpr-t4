56a274a220a3e11085898d3a857d1fba824b9baf
GoranGotovac <ggotovac1@etf.unsa.ba>
2018-11-11 15:15:03 +0100

    Dovrsena klasa PlanStudija sa metodom dodajPredmet

--- src/ba/unsa/etf/rprtutorijal4/PlanStudija.java
+++ src/ba/unsa/etf/rprtutorijal4/PlanStudija.java
@@ -1,13 +1,24 @@
 package ba.unsa.etf.rprtutorijal4;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class PlanStudija {
-    public Map<Integer, ArrayList<Predmet>> mapa;
-    public ArrayList<Predmet> predmeti;
+    public Map<Integer, ArrayList<Predmet>> mapa = new HashMap<>();
+
     public void dodajPredmet(int semestar, Predmet predmet) {
-        predmeti.add(predmet);
-        mapa.put(semestar,predmeti);
+        ArrayList<Predmet> predmeti = new ArrayList<>();
+        if(mapa.isEmpty()) {
+            predmeti.add(predmet);
+            mapa.put(semestar, predmeti);
+        }
+        else {
+            for (Map.Entry<Integer, ArrayList<Predmet>> m: mapa.entrySet()) {
+                if(m.getKey().equals(semestar)) {
+                    m.getValue().add(predmet);
+                }
+            }
+        }
     }
 }
