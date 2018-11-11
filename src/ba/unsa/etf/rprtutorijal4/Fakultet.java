0f48b345d4b6c0a4345891c6c7f08b32f66947aa
GoranGotovac <ggotovac1@etf.unsa.ba>
2018-11-11 15:25:54 +0100

    Dodane metode dodajStudenta, dodajPredmet, setUpis u klasi Fakultet

--- src/ba/unsa/etf/rprtutorijal4/Fakultet.java
+++ src/ba/unsa/etf/rprtutorijal4/Fakultet.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rprtutorijal4;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Fakultet {
+    public Set<Predmet> predmeti = new HashSet<>();
+    public Set<Upis> upisi = new HashSet<>();
+    public Set<Student> studenti = new HashSet<>();
+    public void dodajStudenta(Student student) {
+        studenti.add(student);
+    }
+    public void dodajPredmet(Predmet predmet) {
+        predmeti.add(predmet);
+    }
+    public void setUpis(Upis upis) {
+        predmeti.add(upis.getPredmet());
+        studenti.add(upis.getStudent());
+        upisi.add(upis);
+    }
+
 }
